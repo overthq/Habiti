ab52696ded9b7ec05aaa09a6aaa61830cdd1c951
Oluwakorede Fashokun <koredefashokun@gmail.com>
2020-11-12 23:35:50 +0100

    Remove old API implementation

--- api/src/redisClient.ts
+++
@@ -1,9 +0,0 @@
-import redis from 'redis';
-
-const redisClient = redis.createClient({ url: process.env.REDIS_URL });
-
-redisClient.on('error', error => {
-	throw new Error(error);
-});
-
-export default redisClient;
