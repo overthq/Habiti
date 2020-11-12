1a559420a165f29f1bc45c5ba28007e8baa1a837
Oluwakorede Fashokun <koredefashokun@gmail.com>
2020-11-12 13:41:18 +0100

    Add Redux boilerplate

---
+++ app/src/redux/store.ts
@@ -0,0 +1,23 @@
+import { AsyncStorage } from 'react-native';
+import { persistReducer, persistStore } from 'redux-persist';
+import { createStore, applyMiddleware, combineReducers, Action } from 'redux';
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
+import thunk, { ThunkAction } from 'redux-thunk';
+
+import authReducer from './auth/reducer';
+
+const rootReducer = combineReducers({
+	auth: persistReducer({ key: 'auth', storage: AsyncStorage }, authReducer)
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+	ReturnType,
+	RootState,
+	null,
+	Action<string>
+>;
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const store = createStore(rootReducer, middleware);
+export const persistor = persistStore(store);
