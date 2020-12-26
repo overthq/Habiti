de6a20c0149898321dc5c0ce42fae53e03803eef
Oluwakorede Fashokun <koredefashokun@gmail.com>
2020-12-26 22:37:58 +0100

    Rename Orders to Order

---
+++ app/src/screens/Order.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Order = () => {
+	return (
+		<View style={styles.container}>
+			<Text>Order Details:</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	}
+});
+
+export default Order;
