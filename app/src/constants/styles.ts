928cf6995973e0d0eab6b8cc67808a38f9af3556
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-01-06 12:24:56 +0100

    Create Order query

---
+++ app/src/constants/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const textStyles = StyleSheet.create({
+	screenHeader: {
+		fontSize: 32,
+		fontWeight: 'bold',
+		color: '#000000'
+	},
+	sectionHeader: {
+		fontSize: 16,
+		fontWeight: 'bold',
+		color: '#505050',
+		marginLeft: 16,
+		marginVertical: 8
+	}
+});
