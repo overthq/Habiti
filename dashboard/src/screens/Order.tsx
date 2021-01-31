06454ab207a1b7e909f081c2b67a2db2289b133e
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-01-31 07:37:22 +0100

    Start composing Order screen

--- dashboard/src/screens/Order.tsx
+++ dashboard/src/screens/Order.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import { useOrderQuery } from '../types/api';
 
 // What is on this screen?
 // Order ID/number,
@@ -10,6 +12,16 @@ import { View, StyleSheet } from 'react-native';
 // Fun question (logic-related): What happens if the price of an item changes before the order is fulfilled?
 
 const Order: React.FC = () => {
+	const { params } = useRoute<any>();
+	const { orderId } = params;
+
+	const [{ data }] = useOrderQuery({ variables: { orderId } });
+
+	// Always handle query errors correctly
+	// Use skeleton view for loading state.
+
+	const order = data?.orders[0];
+
 	return <View style={styles.container}></View>;
 };
 
