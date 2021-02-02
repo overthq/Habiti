c838e2810af66cc3653893ac8d45d0ea3347379d
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-02-02 06:41:31 +0100

    Start working on store screen in dashboard

--- app/src/screens/Order.tsx
+++ app/src/screens/Order.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import { useOrderQuery } from '../types/api';
 
 const Order = () => {
+	const { params } = useRoute<any>();
+	const { orderId } = params;
+
+	const [{ data }] = useOrderQuery({ variables: { orderId } });
+
+	const order = data?.orders[0];
+
 	return (
 		<View style={styles.container}>
 			<Text>Order Details:</Text>
+			<Text>{order.id}</Text>
 		</View>
 	);
 };
