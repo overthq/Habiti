e76bce91f47b449657aaadbbd151205b47232963
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-03-12 01:30:25 +0100

    Commit to building dashboard first

--- dashboard/src/screens/Order.tsx
+++ dashboard/src/screens/Order.tsx
@@ -2,8 +2,33 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { useRoute, RouteProp } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useOrderQuery } from '../types/api';
+import { OrderQuery, useOrderQuery } from '../types/api';
 import { OrdersStackParamsList } from '../types/navigation';
+
+interface OrderItemProps {
+	orderItem: OrderQuery['orders'][-1]['order_items'];
+}
+
+const OrderItem: React.FC<OrderItemProps> = ({ orderItem }) => {
+	const { id, item, quantity, unit_price } = orderItem;
+
+	return (
+		<View key={id}>
+			<View
+				style={{
+					height: 50,
+					width: 50,
+					backgroundColor: '#D3D3D3',
+					borderRadius: 4
+				}}
+			/>
+			<Text>{item.name}</Text>
+			<Text>
+				{quantity} - N{quantity * unit_price}
+			</Text>
+		</View>
+	);
+};
 
 const Order: React.FC = () => {
 	const {
@@ -17,7 +42,12 @@ const Order: React.FC = () => {
 	return (
 		<SafeAreaView style={styles.container}>
 			<Text>{order.status}</Text>
-			<View />
+			<View>
+				<Text>Order items</Text>
+				{order.order_items.map(orderItem => (
+					<OrderItem key={id} orderItem={orderItem} />
+				))}
+			</View>
 		</SafeAreaView>
 	);
 };
