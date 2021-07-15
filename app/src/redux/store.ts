e124559192e9138cff85c9e3e1a9523bcb722266
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-07-15 20:34:51 +0100

    Remove all references to client carts

--- app/src/redux/store.ts
+++ app/src/redux/store.ts
@@ -5,12 +5,10 @@ import { TypedUseSelectorHook, useSelector } from 'react-redux';
 import thunk, { ThunkAction } from 'redux-thunk';
 
 import authReducer from './auth/reducer';
-// import cartsReducer from './carts/reducer';
 import preferencesReducer from './preferences/reducer';
 
 const rootReducer = combineReducers({
 	auth: persistReducer({ key: 'auth', storage: AsyncStorage }, authReducer),
-	// carts: persistReducer({ key: 'carts', storage: AsyncStorage }, cartsReducer),
 	preferences: persistReducer(
 		{ key: 'preferences', storage: AsyncStorage },
 		preferencesReducer
