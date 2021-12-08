112d1e1152874b73eb6332819e73fd8a02c46813
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-12-08 02:55:08 -0700

    [Experiment] - Stub resolvers

--- api/src/collections/store/mutations.ts
+++ api/src/collections/store/mutations.ts
@@ -1,8 +1,22 @@
-export const createStore = () => {};
+import { ResolverContext } from '../../types/resolvers';
+
+export const createStore = async (_, { input }, ctx: ResolverContext) => {
+	const store = await ctx.prisma.store.create({
+		data: {
+			name: input.name
+		}
+	});
+
+	return store;
+};
 
 export const followStore = () => {};
 
-export const deleteStore = () => {};
+export const deleteStore = async (_, { id }, ctx: ResolverContext) => {
+	await ctx.prisma.store.delete({ where: { id } });
+
+	return id;
+};
 
 export default {
 	Mutation: {
