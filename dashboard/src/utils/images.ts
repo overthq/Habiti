b66976fddcdaf635efdfaa7ff6de7fbcacc7a544
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-12-18 05:08:08 -0700

    Remove upload image util

--- dashboard/src/utils/images.ts
+++ dashboard/src/utils/images.ts
@@ -1,5 +1,3 @@
-import env from '../../env';
-import * as FileSystem from 'expo-file-system';
 import { ReactNativeFile } from 'extract-files';
 
 export const generateUploadFile = (uri: string) => {
@@ -8,28 +6,4 @@ export const generateUploadFile = (uri: string) => {
 		type: 'image/jpeg',
 		name: 'upload.jpg'
 	});
-};
-
-export const uploadImage = async (
-	uri: string,
-	metadata?: Record<string, string>
-) => {
-	try {
-		const data = await FileSystem.uploadAsync(
-			`${env.apiUrl}/storage/upload`,
-			uri,
-			{
-				httpMethod: 'POST',
-				headers: { Authorization: 'Bearer' },
-				uploadType: FileSystem.FileSystemUploadType.MULTIPART,
-				fieldName: 'file',
-				mimeType: `image/${uri.substring(uri.lastIndexOf('.') + 1)}`,
-				parameters: metadata
-			}
-		);
-
-		return data;
-	} catch (error) {
-		console.log(error);
-	}
 };
