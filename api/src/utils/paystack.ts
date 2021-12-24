722bb90ceeefac2f3ae5467c04dd86e536edbb9f
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-12-24 10:27:45 -0700

    More work on Paystack webhooks

--- api/src/utils/paystack.ts
+++ api/src/utils/paystack.ts
@@ -1,4 +1,7 @@
+import { PrismaClient, User } from '@prisma/client';
 const API_URL = 'https://api.paystack.co';
+
+const prisma = new PrismaClient();
 
 interface ChargeAuthorizationOptions {
 	authorizationCode: string;
@@ -24,4 +27,28 @@ export const chargeAuthorization = async (
 
 	const data = await response.json();
 	return data;
+};
+
+export const storeCard = async (user: User, data: any) => {
+	const card = await prisma.card.findUnique({
+		where: { signature: data.authorization.signature }
+	});
+
+	if (!card) {
+		await prisma.card.create({
+			data: {
+				email: data.customer.email,
+				authorizationCode: data.authorization.authorization_code,
+				bin: data.authorization.bin,
+				last4: data.authorization.last4,
+				expMonth: data.authorization.exp_month,
+				expYear: data.authorization.exp_year,
+				bank: data.authorization.bank,
+				signature: data.authorization.signature,
+				cardType: data.authorization.card_type,
+				countryCode: data.authorization.country_code,
+				userId: user.id
+			}
+		});
+	}
 };
--- api/src/webhooks.ts
+++ api/src/webhooks.ts
@@ -1,26 +1,29 @@
 import { Router } from 'express';
 import { createHmac } from 'crypto';
+import { User } from '@prisma/client';
+import { storeCard } from './utils/paystack';
 
 const router = Router();
 
-router.post('/paystack', (req, res) => {
+router.post('/paystack', async (req, res) => {
 	const hash = createHmac('sha512', process.env.PAYSTACK_SECRET_KEY)
 		.update(JSON.stringify(req.body))
 		.digest('hex');
 
 	if (hash == req.headers['x-paystack-signature']) {
 		const { event, data } = req.body;
-		if (event === 'charge.success') {
-			console.log(data);
-		}
-	}
 
-	return res.status(200).json({
-		success: true,
-		data: {
-			message: 'Done.'
+		if (event === 'charge.success') {
+			await storeCard(req.user as User, data);
 		}
-	});
+
+		return res.status(200).json({
+			success: true,
+			data: {
+				message: 'Done.'
+			}
+		});
+	}
 });
 
 export default router;
