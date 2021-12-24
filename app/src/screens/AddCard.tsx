189ee3a5987f6fa042629ce01cdcfcdba10f6105
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-12-23 19:16:56 -0700

    Start working on Paystack integration

---
+++ app/src/screens/AddCard.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Formik } from 'formik';
+import Button from '../components/global/Button';
+
+const AddCard = () => {
+	return (
+		<View style={styles.container}>
+			<Formik
+				initialValues={{
+					cardNumber: '',
+					cvv: '',
+					expiryMonth: '',
+					expiryYear: ''
+				}}
+				onSubmit={values => {
+					console.log(values);
+				}}
+			>
+				{({ values, handleChange, handleBlur, handleSubmit }) => (
+					<>
+						<TextInput
+							value={values.cardNumber}
+							onChangeText={handleChange('cardNumber')}
+							onBlur={handleBlur('cardNumber')}
+						/>
+						<TextInput
+							value={values.expiryMonth}
+							onChangeText={handleChange('expiryMonth')}
+							onBlur={handleBlur('expiryMonth')}
+						/>
+						<TextInput
+							value={values.expiryYear}
+							onChangeText={handleChange('expiryYear')}
+							onBlur={handleBlur('expiryYear')}
+						/>
+						<TextInput
+							value={values.cvv}
+							onChangeText={handleChange('cvv')}
+							onBlur={handleBlur('cvv')}
+						/>
+						<Button text='Add Card' onPress={handleSubmit} />
+					</>
+				)}
+			</Formik>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	}
+});
+
+export default AddCard;
---
+++ app/src/screens/PaymentMethods.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const PaymentMethods: React.FC = () => {
+	return <View></View>;
+};
+
+export default PaymentMethods;
