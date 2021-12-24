62216f5927b7a225408a9f0bf45f6942b0a57394
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-12-23 19:02:53 -0700

    Remove mono

--- app/App.tsx
+++ app/App.tsx
@@ -3,20 +3,16 @@ import React from 'react';
 import { Provider as StateProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-// import { MonoProvider } from '@mono.co/connect-react-native';
 
 import { store, persistor } from './src/redux/store';
 import Routes from './src/navigation/Routes';
-// import monoConfig from './src/utils/mono';
 
 const App: React.FC = () => (
 	<StateProvider store={store}>
 		<PersistGate persistor={persistor}>
-			{/* <MonoProvider {...monoConfig}> */}
 			<SafeAreaProvider>
 				<Routes />
 			</SafeAreaProvider>
-			{/* </MonoProvider> */}
 		</PersistGate>
 	</StateProvider>
 );
