599c04e481553b0cd178534c4a5c8f352ddc958a
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-12-29 02:51:04 -0700

    Charging the card works (kind of)

--- api/src/collections/order/mutations.ts
+++ api/src/collections/order/mutations.ts
@@ -1,15 +1,41 @@
 import { Resolver } from '../../types/resolvers';
+import { chargeAuthorization } from '../../utils/paystack';
 
-export const createOrder: Resolver<any> = async (_, { cartId }, ctx) => {
+// TODO: Pass cardId here (and verify that the user owns the card).
+interface CreateOrderArgs {
+	cartId: string;
+	// cardId: string;
+}
+
+export const createOrder: Resolver<CreateOrderArgs> = async (
+	_,
+	{ cartId },
+	ctx
+) => {
 	const cart = await ctx.prisma.cart.findUnique({
 		where: { id: cartId },
 		include: {
+			user: {
+				include: {
+					cards: true
+				}
+			},
 			products: {
 				include: {
 					product: true
 				}
 			}
 		}
+	});
+
+	const total = cart.products.reduce((acc, next) => {
+		return acc + next.product.unitPrice * next.quantity;
+	}, 0);
+
+	await chargeAuthorization({
+		email: 'test@test.co',
+		amount: String(total * 100),
+		authorizationCode: cart.user.cards[0].authorizationCode
 	});
 
 	if (cart.userId === ctx.user.id) {
@@ -27,6 +53,12 @@ export const createOrder: Resolver<any> = async (_, { cartId }, ctx) => {
 				}
 			}
 		});
+
+		// Delete the cart.
+		await ctx.prisma.cart.delete({
+			where: { id: cartId }
+		});
+
 		return order;
 	}
 };
