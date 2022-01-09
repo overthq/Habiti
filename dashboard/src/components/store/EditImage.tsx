52329a82c56bde4c2f92502f1d2da20cfce0b43a
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-01-08 17:02:37 -0700

    Start working on store image edit

---
+++ dashboard/src/components/store/EditImage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// Allow both image removal and replacement.
+
+const EditImage: React.FC = () => {
+	return <View></View>;
+};
+
+export default EditImage;
--- dashboard/src/screens/EditStore.tsx
+++ dashboard/src/screens/EditStore.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 import { Formik } from 'formik';
 import Button from '../components/global/Button';
 
@@ -9,7 +9,10 @@ const EditStore: React.FC = () => {
 			<Formik
 				initialValues={{
 					name: '',
-					description: ''
+					description: '',
+					website: '',
+					twitter: '',
+					instagram: ''
 				}}
 				onSubmit={values => {
 					console.log(values);
@@ -18,22 +21,55 @@ const EditStore: React.FC = () => {
 				{({ values, handleChange, handleBlur, handleSubmit }) => (
 					<>
 						<View style={styles.field}>
+							<Text style={styles.label}>Name</Text>
 							<TextInput
 								style={styles.input}
+								placeholder='Name'
 								value={values.name}
 								onChangeText={handleChange('name')}
 								onBlur={handleBlur('name')}
 							/>
 						</View>
 						<View style={styles.field}>
+							<Text style={styles.label}>Website</Text>
 							<TextInput
 								style={styles.input}
+								placeholder='https://acme.com'
+								value={values.website}
+								onChangeText={handleChange('website')}
+								onBlur={handleBlur('website')}
+							/>
+						</View>
+						<View style={styles.field}>
+							<Text style={styles.label}>Description</Text>
+							<TextInput
+								style={[styles.input, { height: 80 }]}
 								placeholder='Description'
 								value={values.description}
 								onChangeText={handleChange('description')}
 								onBlur={handleBlur('description')}
 								multiline
 								textAlignVertical='top'
+							/>
+						</View>
+						<View style={styles.field}>
+							<Text style={styles.label}>Twitter username</Text>
+							<TextInput
+								style={styles.input}
+								placeholder='@acme_inc'
+								value={values.twitter}
+								onChangeText={handleChange('twitter')}
+								onBlur={handleBlur('twitter')}
+							/>
+						</View>
+						<View style={styles.field}>
+							<Text style={styles.label}>Instagram username</Text>
+							<TextInput
+								style={styles.input}
+								placeholder='@acme'
+								value={values.instagram}
+								onChangeText={handleChange('instagram')}
+								onBlur={handleBlur('instagram')}
 							/>
 						</View>
 						<Button
