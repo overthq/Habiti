034badeb827109c0266b39e70a7233f73702ca50
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-01-15 00:25:40 -0700

    Remove date util test log

--- app/src/utils/date.ts
+++ app/src/utils/date.ts
@@ -1,8 +1,6 @@
 import { fromUnixTime } from 'date-fns';
 import format from 'date-fns/format';
 
-console.log(format(new Date(), 'dd LLL'));
-
 // Something like "29 Sep"? (at least, for now)
 export const relativeTimestamp = (timestamp: string) => {
 	return format(fromUnixTime(Number(timestamp)), 'dd LLL');
