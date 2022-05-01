b3158faf28d213c5b4050489fc224a845630bad1
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-05-01 02:36:26 -0400

    Start working on stats code

--- api/src/collections/stats/queries.ts
+++ api/src/collections/stats/queries.ts
@@ -1,4 +1,25 @@
+import { Resolver } from '../../types/resolvers';
+
+// TODO:
+// - Normalize the "period" data to get a date range.
+// - Use the date range to fetch the needed data.
+// - Probably fetch all the data in one query (or use a transaction).
+// - Return the data in the correct format.
+// - Consider caching results (maybe not though, since we need live data).
+
+interface StatsArgs {
+	storeId: string;
+	period: 'Day' | 'Week' | 'Month' | 'Year';
+}
+
+const stats: Resolver<StatsArgs> = async (_, { storeId, period }) => {
+	console.log(storeId, period);
+	return null;
+};
+
 export default {
 	Stats: {},
-	Query: {}
+	Query: {
+		stats
+	}
 };
--- api/src/collections/stats/types.ts
+++ api/src/collections/stats/types.ts
@@ -1,5 +1,24 @@
 import { gql } from 'apollo-server-express';
 
-const StatsTypes = gql``;
+const StatsTypes = gql`
+	type Stats {
+		storeId: ID!
+		store: Store!
+		revenue: ID!
+		pendingOrders: Int!
+		newOrders: Int!
+	}
+
+	enum StatPeriod {
+		Day
+		Week
+		Month
+		Year
+	}
+
+	extend type Query {
+		stats(storeId: ID!, period: StatPeriod): Stats!
+	}
+`;
 
 export default StatsTypes;
