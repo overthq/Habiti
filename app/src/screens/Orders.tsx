b86c0111b6d9f1fdd83da2f1710783c8dfab46f2
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-07-20 23:33:31 -0400

    Start scaffolding orders screen

--- app/src/screens/Orders.tsx
+++ app/src/screens/Orders.tsx
@@ -1,9 +1,10 @@
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
 import { FlatList, View, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 import { useUserOrdersQuery } from '../types/api';
 import { AppStackParamList } from '../types/navigation';
+import OrdersListItem from '../components/orders/OrdersListItem';
 
 // This screen is an in-depth list of orders.
 // It should support filtering and bulk actions (maybe).
@@ -12,13 +13,23 @@ const Orders: React.FC = () => {
 	const [{ data, fetching }] = useUserOrdersQuery();
 	const { navigate } = useNavigation<StackNavigationProp<AppStackParamList>>();
 
-	const handleOrderPress = React.useCallback((orderId: string) => {
-		navigate('Order', { orderId });
-	}, []);
+	const handleOrderPress = React.useCallback(
+		(orderId: string) => () => {
+			navigate('Order', { orderId });
+		},
+		[]
+	);
+
+	if (fetching) return <View />;
 
 	return (
 		<View style={styles.container}>
-			<FlatList data={data?.currentUser.orders} renderItem={() => <View />} />
+			<FlatList
+				data={data?.currentUser.orders}
+				renderItem={({ item }) => (
+					<OrdersListItem order={item} onPress={handleOrderPress(item.id)} />
+				)}
+			/>
 		</View>
 	);
 };
