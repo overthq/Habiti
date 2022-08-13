197485caed0173d81d8a5b286bac1de35e09eda1
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-08-12 22:37:56 -0400

    Swap multiple queries for transactions

--- api/src/collections/stats/queries.ts
+++ api/src/collections/stats/queries.ts
@@ -1,12 +1,5 @@
 import { Resolver } from '../../types/resolvers';
 
-// TODO:
-// - Normalize the "period" data to get a date range.
-// - Use the date range to fetch the needed data.
-// - Probably fetch all the data in one query (or use a transaction).
-// - Return the data in the correct format.
-// - Consider caching results (maybe not though, since we need live data).
-
 enum StatPeriod {
 	Day = 'Day',
 	Week = 'Week',
@@ -37,59 +30,35 @@ interface StatsArgs {
 const stats: Resolver<StatsArgs> = async (_, { storeId, period }, ctx) => {
 	const marker = getDateFromPeriod(period);
 
-	const products = await ctx.prisma.product.findMany({
-		where: {
-			storeId,
-			createdAt: { gte: marker }
-		}
-	});
-
-	const orders = await ctx.prisma.order.findMany({
-		where: {
-			storeId,
-			createdAt: { gte: marker }
-		}
-	});
-
-	// This does not work, because we need to add the order "total"
-	// to the orders table, instead of relying on computing it from the
-	// order products every time.
-	// However, if we do this, it means that that column has to be updated
-	// every time an order product is deleted/updated.
-
-	const projectedRevenue = await ctx.prisma.order.aggregate({
-		where: {
-			storeId,
-			createdAt: { gte: marker }
-		},
-		_count: {
-			_all: true
-		}
-	});
-
-	await ctx.prisma.orderProduct.aggregate({
-		where: {
-			order: {
+	const [products, orders, projectedRevenue] = await ctx.prisma.$transaction([
+		ctx.prisma.product.findMany({
+			where: {
 				storeId,
 				createdAt: { gte: marker }
 			}
-		},
-		_count: {
-			unitPrice: true
-		}
-	});
+		}),
+
+		ctx.prisma.order.findMany({
+			where: {
+				storeId,
+				createdAt: { gte: marker }
+			}
+		}),
+		ctx.prisma.orderProduct.aggregate({
+			where: {
+				order: {
+					storeId,
+					createdAt: { gte: marker }
+				}
+			},
+			_count: {
+				unitPrice: true
+			}
+		})
+	]);
 
 	return { products, orders, projectedRevenue };
 };
-
-// const pendingOrders : Resolver = async () => {
-// }
-
-// const newOrders: Resolver = async () => {
-// }
-
-// const revenue: Resolver = async () => {
-// };
 
 const id: Resolver = parent => {
 	return parent.storeId;
