359f7c00ca9de5bd8b13a6176201a127865bf987
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-08-15 00:36:40 -0400

    Add PeriodSelector

---
+++ dashboard/src/components/overview/PeriodSelector.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { View, Pressable, Text, StyleSheet } from 'react-native';
+import { StatPeriod } from '../../types/api';
+
+interface PeriodSelectorProps {
+	selectedPeriod: StatPeriod;
+	setSelectedPeriod(period: StatPeriod): void;
+}
+
+const options = [
+	StatPeriod.Day,
+	StatPeriod.Week,
+	StatPeriod.Month,
+	StatPeriod.Year
+];
+
+const PeriodSelector: React.FC<PeriodSelectorProps> = ({
+	selectedPeriod,
+	setSelectedPeriod
+}) => {
+	const handlePeriodSelect = (p: StatPeriod) => () => {
+		setSelectedPeriod(p);
+	};
+
+	return (
+		<View style={styles.container}>
+			{options.map(p => (
+				<Pressable
+					key={p}
+					style={styles.tab}
+					onPress={handlePeriodSelect(p)}
+					disabled={p === selectedPeriod}
+				>
+					<Text style={styles.text}>{p}</Text>
+				</Pressable>
+			))}
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		width: '100%',
+		height: 40,
+		flexDirection: 'row'
+	},
+	tab: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	text: {
+		fontSize: 16
+	}
+});
+
+export default PeriodSelector;
--- dashboard/src/screens/Overview.tsx
+++ dashboard/src/screens/Overview.tsx
@@ -11,12 +11,14 @@ import { Icon } from '../components/Icon';
 import { AppStackParamList } from '../types/navigation';
 import { StatPeriod, useStatsQuery } from '../types/api';
 import useStore from '../state';
+import PeriodSelector from '../components/overview/PeriodSelector';
 
 const Overview: React.FC = () => {
 	const navigation = useNavigation<NavigationProp<AppStackParamList>>();
+	const [selectedPeriod, setSelectedPeriod] = React.useState(StatPeriod.Week);
 	const activeStore = useStore(({ activeStore }) => activeStore);
 	const [{ fetching, data }] = useStatsQuery({
-		variables: { storeId: activeStore as string, period: StatPeriod.Week }
+		variables: { storeId: activeStore as string, period: selectedPeriod }
 	});
 
 	React.useLayoutEffect(() => {
@@ -36,6 +38,10 @@ const Overview: React.FC = () => {
 
 	return (
 		<ScrollView style={styles.container}>
+			<PeriodSelector
+				selectedPeriod={selectedPeriod}
+				setSelectedPeriod={setSelectedPeriod}
+			/>
 			<Text>Orders: {data?.stats.orders.length ?? 0}</Text>
 		</ScrollView>
 	);
