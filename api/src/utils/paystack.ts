3f25a963537751c822cb60686156cc7e0a4f72a8
Oluwakorede Fashokun <koredefashokun@gmail.com>
2023-01-15 00:05:11 -0500

    Start working Paystack payouts

--- api/src/utils/paystack.ts
+++ api/src/utils/paystack.ts
@@ -77,8 +77,6 @@ export const initialCharge = async (email: string) => {
 // (On prod, the webhook should do this).
 
 export const verifyTransaction = async (userId: string, reference: string) => {
-	console.log('here too');
-	console.log(`${API_URL}/transaction/verify/${reference}`);
 	const response = await fetch(`${API_URL}/transaction/verify/${reference}`, {
 		method: 'GET',
 		headers: {
@@ -86,11 +84,7 @@ export const verifyTransaction = async (userId: string, reference: string) => {
 		}
 	});
 
-	const v = await response.json();
-
-	console.log(v);
-
-	const { data, status } = v;
+	const { data, status } = await response.json();
 
 	if (status === true && data.status === 'success') {
 		const card = await storeCard(userId, data);
@@ -98,4 +92,61 @@ export const verifyTransaction = async (userId: string, reference: string) => {
 	} else {
 		throw new Error('Verification failed!');
 	}
+};
+
+export const createRecepient = async (
+	name: string,
+	accountNumber: string,
+	bankCode: string
+) => {
+	// Consider also doing this with the authorization code.
+
+	const response = await fetch(`${API_URL}/transferrecepient`, {
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			type: 'nuban',
+			name,
+			account_number: accountNumber,
+			bank_code: bankCode,
+			currency: 'NGN'
+		})
+	});
+
+	const { data } = await response.json();
+
+	return data;
+};
+
+export const createTransferReference = () => {
+	// Do something.
+};
+
+export const payAccount = async (amount: string, reference: string) => {
+	// Pay account.
+	const response = await fetch(`${API_URL}`, {
+		headers: {
+			Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`
+		},
+		body: JSON.stringify({ amount, reference })
+	});
+
+	const data = await response.json();
+
+	return data;
+};
+
+export const verifyTransfer = async (transferId: string) => {
+	const response = await fetch(`${API_URL}/transfer/${transferId}`, {
+		headers: {
+			Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`
+		}
+	});
+
+	const data = await response.json();
+
+	return data;
 };
