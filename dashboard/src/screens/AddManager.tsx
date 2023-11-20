896219cf69f980f4826d8d14dd6ad7be1d74477d
Oluwakorede Fashokun <koredefashokun@gmail.com>
2023-11-19 21:05:22 -0500

    More stuff

--- dashboard/src/screens/AddManager.tsx
+++ dashboard/src/screens/AddManager.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import Screen from '../components/global/Screen';
 import useGoBack from '../hooks/useGoBack';
+import Button from '../components/global/Button';
 
 const AddManager = () => {
 	useGoBack('x');
 
-	return <Screen></Screen>;
+	const { handleSubmit } = useForm();
+
+	const onSubmit = React.useCallback(() => {
+		// Things
+	}, []);
+
+	return (
+		<Screen>
+			<Button text='Submit' onPress={handleSubmit(onSubmit)} />
+		</Screen>
+	);
 };
 
 export default AddManager;
