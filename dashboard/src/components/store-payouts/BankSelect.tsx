0ef07f33bbf3491058d3ae35687eeb4c3cf1d340
Oluwakorede Fashokun <koredefashokun@gmail.com>
2023-12-29 09:21:33 -0500

    Close modal on bank select

--- dashboard/src/components/store-payouts/BankSelect.tsx
+++ dashboard/src/components/store-payouts/BankSelect.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Pressable, StyleSheet } from 'react-native';
-// import { AppStackParamList } from '../../types/navigation';
-// import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { BottomSheetFlatList, BottomSheetModal } from '@gorhom/bottom-sheet';
 import { BANKS } from '../../utils/banks';
 import Typography from '../global/Typography';
@@ -15,10 +13,14 @@ interface BankSelectProps {
 
 const BankSelect: React.FC<BankSelectProps> = ({ modalRef, setBank }) => {
 	const { theme } = useTheme();
-	// const { navigate } = useNavigation<NavigationProp<AppStackParamList>>();
 
-	// const handleBankSelect = () => {
-	// };
+	const handleBankSelect = React.useCallback(
+		(code: string) => () => {
+			setBank(code);
+			modalRef.current?.close();
+		},
+		[modalRef.current]
+	);
 
 	const snapPoints = React.useMemo(() => ['25%', '50%', '90%'], []);
 
@@ -34,7 +36,7 @@ const BankSelect: React.FC<BankSelectProps> = ({ modalRef, setBank }) => {
 				data={BANKS}
 				keyExtractor={b => b.id.toString()}
 				renderItem={({ item }) => (
-					<Pressable style={styles.row} onPress={() => setBank(item.code)}>
+					<Pressable style={styles.row} onPress={handleBankSelect(item.code)}>
 						<Typography>{item.name}</Typography>
 					</Pressable>
 				)}
