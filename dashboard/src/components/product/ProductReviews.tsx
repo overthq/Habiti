41553affedf1ca446378e27a07825b0c9b3fe636
Oluwakorede Fashokun <koredefashokun@gmail.com>
2024-02-22 17:07:54 -0500

    Add component for product reviews section

---
+++ dashboard/src/components/product/ProductReviews.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import Typography from '../global/Typography';
+
+const ProductReviews = () => {
+	return (
+		<View>
+			<Typography weight='medium'>Reviews</Typography>
+		</View>
+	);
+};
+
+export default ProductReviews;
