b1c00061eb83c0c0def44b27ac14a4e9b05852a9
Oluwakorede Fashokun <koredefashokun@gmail.com>
2024-03-06 04:12:33 -0500

    Add custom header component to Explore screen

--- apps/app/src/components/explore/ExploreHeader.tsx
+++ apps/app/src/components/explore/ExploreHeader.tsx
@@ -1,6 +1,8 @@
-import { TextButton } from '@market/components';
+import { Icon, TextButton, useTheme } from '@market/components';
 import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
+import Animated from 'react-native-reanimated';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 interface ExploreHeaderProps {
 	searchOpen: boolean;
@@ -11,20 +13,65 @@ const ExploreHeader: React.FC<ExploreHeaderProps> = ({
 	searchOpen,
 	setSearchOpen
 }) => {
+	const { top } = useSafeAreaInsets();
+	const { theme } = useTheme();
+
+	const inputRef = React.useRef<TextInput>(null);
+
+	const blurInput = React.useCallback(() => {
+		inputRef.current?.blur();
+	}, [inputRef.current]);
+
+	const handleBlur = () => {
+		setSearchOpen(false);
+	};
+
+	const handleFocus = () => {
+		setSearchOpen(true);
+	};
+
 	return (
-		<View style={styles.container}>
-			<View>
-				<TextInput />
-			</View>
+		<View style={[styles.container, { paddingTop: top }]}>
+			<Animated.View
+				style={[styles.input, { backgroundColor: theme.input.background }]}
+			>
+				<Icon name='search' size={18} color={theme.text.secondary} />
+				<TextInput
+					ref={inputRef}
+					placeholder='Search products and stores'
+					placeholderTextColor={theme.text.secondary}
+					style={styles.inputText}
+					onBlur={handleBlur}
+					onFocus={handleFocus}
+				/>
+			</Animated.View>
 			{searchOpen ? (
-				<TextButton onPress={() => setSearchOpen(false)}>Cancel</TextButton>
+				<View style={{ marginLeft: 12 }}>
+					<TextButton onPress={blurInput}>Cancel</TextButton>
+				</View>
 			) : null}
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
-	container: {}
+	container: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		paddingHorizontal: 16
+	},
+	input: {
+		flexDirection: 'row',
+		flex: 1,
+		alignItems: 'center',
+		borderRadius: 6,
+		padding: 8
+	},
+	inputText: {
+		flex: 1,
+		marginLeft: 8,
+		fontSize: 16
+	}
 });
 
 export default ExploreHeader;
--- apps/app/src/components/explore/RecentSearches.tsx
+++ apps/app/src/components/explore/RecentSearches.tsx
@@ -1,8 +1,15 @@
+import { Typography } from '@market/components';
 import React from 'react';
 import { View } from 'react-native';
 
 const RecentSearches = () => {
-	return <View></View>;
+	return (
+		<View>
+			<Typography weight='medium' variant='label' size='large'>
+				Recent searches
+			</Typography>
+		</View>
+	);
 };
 
 export default RecentSearches;
