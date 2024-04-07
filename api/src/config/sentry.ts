f0858a4e689eec3fa8c685d11797251bf2f131c9
Oluwakorede Fashokun <koredefashokun@gmail.com>
2024-04-06 22:16:12 -0400

    Clean up Sentry config

--- api/src/config/sentry.ts
+++ api/src/config/sentry.ts
@@ -6,15 +6,11 @@ export const initSentry = (app: Express) => {
 	Sentry.init({
 		dsn: process.env.SENTRY_DSN,
 		integrations: [
-			// enable HTTP calls tracing
 			new Sentry.Integrations.Http({ tracing: true }),
-			// enable Express.js middleware tracing
 			new Sentry.Integrations.Express({ app }),
 			nodeProfilingIntegration()
 		],
-		// Performance Monitoring
-		tracesSampleRate: 1.0, //  Capture 100% of the transactions
-		// Set sampling rate for profiling - this is relative to tracesSampleRate
+		tracesSampleRate: 1.0,
 		profilesSampleRate: 1.0
 	});
 
