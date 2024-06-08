2150f9d96c6accc9e39582d35b6a824ee05fcd1f
Oluwakorede Fashokun <koredefashokun@gmail.com>
2024-06-07 22:36:42 -0400

    Add ManagerSettings screen

---
+++ apps/dashboard/src/screens/ManagerSettings.tsx
@@ -0,0 +1,12 @@
+import { Screen, Typography } from '@market/components';
+import React from 'react';
+
+const ManagerSettings = () => {
+	return (
+		<Screen>
+			<Typography>Manager settings</Typography>
+		</Screen>
+	);
+};
+
+export default ManagerSettings;
