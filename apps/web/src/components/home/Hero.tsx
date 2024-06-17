cd3759e2d443be2ec95bf05032c1e6ce41821673
Oluwakorede Fashokun <koredefashokun@gmail.com>
2024-06-17 06:18:14 -0400

    Add waitlist form link

--- apps/web/src/components/home/Hero.tsx
+++ apps/web/src/components/home/Hero.tsx
@@ -8,22 +8,28 @@ const Hero = () => {
 				Habiti helps shoppers and merchants manage their shopping activity in
 				one place.
 			</p>
-			<button
-				style={{
-					fontFamily: 'Inter',
-					marginTop: 16,
-					padding: '12px 24px',
-					backgroundColor: '#108910',
-					borderRadius: 4,
-					color: '#FFFFFF',
-					fontSize: '1.1rem',
-					fontWeight: '500',
-					border: 'none',
-					cursor: 'pointer'
-				}}
+			<a
+				href='https://forms.gle/VXQjP7gcMJvgZivA6'
+				target='_blank'
+				rel='noopener noreferrer'
 			>
-				Request access
-			</button>
+				<button
+					style={{
+						fontFamily: 'Inter',
+						marginTop: 16,
+						padding: '12px 24px',
+						backgroundColor: '#108910',
+						borderRadius: 4,
+						color: '#FFFFFF',
+						fontSize: '1.1rem',
+						fontWeight: '500',
+						border: 'none',
+						cursor: 'pointer'
+					}}
+				>
+					Request access
+				</button>
+			</a>
 		</div>
 	);
 };
