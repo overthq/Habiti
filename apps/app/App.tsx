630ff9470212cfac05b496ffdbd7650a0a96e67b
Oluwakorede Fashokun <koredefashokun@gmail.com>
2024-06-17 18:49:48 -0400

    Add Sentry DSNs

--- apps/app/App.tsx
+++ apps/app/App.tsx
@@ -1,11 +1,16 @@
 import 'core-js/full/symbol/async-iterator';
 import { ThemeProvider } from '@habiti/components';
+import * as Sentry from '@sentry/react-native';
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import Routes from './src/navigation/Routes';
 import useStore from './src/state';
+
+Sentry.init({
+	dsn: process.env.SENTRY_DSN
+});
 
 const App: React.FC = () => {
 	const theme = useStore(({ theme }) => theme);
