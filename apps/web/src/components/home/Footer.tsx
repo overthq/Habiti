8df627b71b7aa80bb85e269f474e5b556889f87b
Oluwakorede Fashokun <koredefashokun@gmail.com>
2024-06-17 06:08:12 -0400

    Simple website

---
+++ apps/web/src/components/home/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+	return (
+		<footer style={{ padding: 16 }}>
+			<div>
+				<p>&copy; {new Date().getFullYear()} Habiti.</p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
---
+++ apps/web/src/components/home/Hero.tsx
@@ -0,0 +1,31 @@
+const Hero = () => {
+	return (
+		<div style={{ padding: '100px 0', textAlign: 'center' }}>
+			<h1 style={{ fontSize: '2.8125rem', fontWeight: '700' }}>
+				Online shopping, simplified.
+			</h1>
+			<p style={{ marginTop: 16, fontSize: '1.2rem' }}>
+				Habiti helps shoppers and merchants manage their shopping activity in
+				one place.
+			</p>
+			<button
+				style={{
+					fontFamily: 'Inter',
+					marginTop: 16,
+					padding: '12px 24px',
+					backgroundColor: '#108910',
+					borderRadius: 4,
+					color: '#FFFFFF',
+					fontSize: '1.1rem',
+					fontWeight: '500',
+					border: 'none',
+					cursor: 'pointer'
+				}}
+			>
+				Request access
+			</button>
+		</div>
+	);
+};
+
+export default Hero;
