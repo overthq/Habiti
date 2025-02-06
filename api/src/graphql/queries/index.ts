8a7c072814862efdab7201e011a7dce02ff10781
Oluwakorede Fashokun <koredefashokun@gmail.com>
2025-02-05 23:24:58 -0500

    Clean up queries barrel file

--- api/src/graphql/queries/index.ts
+++ api/src/graphql/queries/index.ts
@@ -6,32 +6,25 @@ import storeQueries from './stores';
 import userQueries from './users';
 import searchQueries from './search';
 
-export default {
-	Query: {
-		...cardQueries.Query,
-		...cartQueries.Query,
-		...orderQueries.Query,
-		...productQueries.Query,
-		...storeQueries.Query,
-		...userQueries.Query,
-		...searchQueries.Query
-	},
-	Card: {
-		...cardQueries.Card
-	},
-	Cart: {
-		...cartQueries.Cart
-	},
-	Order: {
-		...orderQueries.Order
-	},
-	Product: {
-		...productQueries.Product
-	},
-	Store: {
-		...storeQueries.Store
-	},
-	User: {
-		...userQueries.User
-	}
+// Combine all Query resolvers
+const Query = {
+	...cardQueries.Query,
+	...cartQueries.Query,
+	...orderQueries.Query,
+	...productQueries.Query,
+	...storeQueries.Query,
+	...userQueries.Query,
+	...searchQueries.Query
 };
+
+// Combine all field resolvers
+const fieldResolvers = {
+	Card: cardQueries.Card,
+	Cart: cartQueries.Cart,
+	Order: orderQueries.Order,
+	Product: productQueries.Product,
+	Store: storeQueries.Store,
+	User: userQueries.User
+};
+
+export { Query, fieldResolvers };
