463ca74fae8b935e8f6c3cc534d3167fff4b5164
Oluwakorede Fashokun <koredefashokun@gmail.com>
2025-05-02 17:09:59 -0400

    Continue adding missing dependencies

--- apps/dashboard/app.config.ts
+++ apps/dashboard/app.config.ts
@@ -1,4 +1,4 @@
-import { ExpoConfig, ConfigContext } from '@expo/config';
+import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
 	...config,
