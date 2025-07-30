3f94b511d3b9b7f0e28e4041c0313f08e0e0b683
Oluwakorede Fashokun <koredefashokun@gmail.com>
2025-07-30 14:37:45 -0400

    Add transfer verification endpoint

--- api/src/routes/payments.ts
+++ api/src/routes/payments.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+import { PayoutStatus } from '@prisma/client';
 
 import { verifyTransaction, verifyTransfer } from '../core/payments';
+import prismaClient from '../config/prisma';
 
 const paymentsRouter: Router = Router();
 
@@ -23,6 +25,29 @@ paymentsRouter.post('/verify-transfer', async (req, res) => {
 		const data = await verifyTransfer({ transferId });
 
 		return res.status(200).json({ success: true, data });
+	} catch (error) {
+		return res.status(400).json({ success: false, error });
+	}
+});
+
+paymentsRouter.post('/approve-payment', async (req, res) => {
+	const { body } = req;
+
+	try {
+		// IMPORTANT: We have to ensure that this action stays as fast as possible.
+		// Any latency in our DB calls here will cause the payout to fail.
+		const payout = await prismaClient.payout.findUnique({
+			where: { id: body.data.reference, status: PayoutStatus.Pending }
+		});
+
+		if (!payout) {
+			return res.status(400).json({
+				success: false,
+				message: 'Payout not found or has already been resolved'
+			});
+		}
+
+		return res.status(200).json({ sucess: true, message: 'Payment approved' });
 	} catch (error) {
 		return res.status(400).json({ success: false, error });
 	}
