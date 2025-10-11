8eeb3d2badf2813b3c9161701c78806f449fa7fc
Oluwakorede Fashokun <koredefashokun@gmail.com>
2025-10-10 22:16:19 -0400

    Remove GraphQL from web

--- apps/web/src/app/(main)/home/page.tsx
+++ apps/web/src/app/(main)/home/page.tsx
@@ -2,157 +2,111 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { gql, useQuery } from 'urql';
 import { cn } from '@/lib/utils';
 import { formatDateFromTimestamp } from '@/utils/date';
+import { useFollowedStoresQuery, useOrdersQuery } from '@/data/queries';
 
-const HomePage = () => {
-	const [{ error, data, fetching }] = useQuery({ query: HOME_QUERY });
+const RecentOrders = () => {
+	const { isLoading, error, data } = useOrdersQuery();
 
-	if (fetching) return <div>Loading...</div>;
+	if (isLoading) return <div>Loading...</div>;
 	if (error) return <div>Error: {error.message}</div>;
+	if (!data) return null;
 
 	return (
-		<div>
-			<h1 className='text-2xl font-medium mb-4'>Recent Orders</h1>
+		<div className='mb-8 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
+			{data.orders.map(order => (
+				<Link href={`/orders/${order.id}`} key={order.id} className='block'>
+					<div className='h-full flex justify-between items-center p-4 rounded-lg border'>
+						<div className='flex gap-4 items-center'>
+							<div className='size-14 rounded-full overflow-hidden bg-muted flex-shrink-0'>
+								{order.store.image && (
+									<img
+										src={order.store.image?.path}
+										alt={order.store.name}
+										className='size-full object-cover'
+									/>
+								)}
+							</div>
 
-			<div className='mb-8 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
-				{data.currentUser.orders.map((order: any) => (
-					<Link href={`/orders/${order.id}`} key={order.id} className='block'>
-						<div className='h-full flex justify-between items-center p-4 rounded-lg border'>
-							<div className='flex gap-4 items-center'>
-								<div className='size-14 rounded-full overflow-hidden bg-muted flex-shrink-0'>
-									{order.store.image && (
-										<img
-											src={order.store.image?.path}
-											alt={order.store.name}
-											className='size-full object-cover'
-										/>
-									)}
-								</div>
-
-								<div>
-									<p className='font-medium'>{order.store.name}</p>
-									<p className='text-sm text-muted-foreground'>
-										{formatDateFromTimestamp(order.createdAt)}
-									</p>
-									<p className='text-sm font-medium mt-1'>
-										₦{order.total.toLocaleString()} ·
-										<span
-											className={cn(
-												'capitalize ml-1',
-												order.status === 'Completed'
-													? 'text-green-600'
-													: 'text-amber-600'
-											)}
-										>
-											{order.status}
-										</span>
-									</p>
-								</div>
+							<div>
+								<p className='font-medium'>{order.store.name}</p>
+								<p className='text-sm text-muted-foreground'>
+									{formatDateFromTimestamp(order.createdAt)}
+								</p>
+								<p className='text-sm font-medium mt-1'>
+									₦{order.total.toLocaleString()} ·
+									<span
+										className={cn(
+											'capitalize ml-1',
+											order.status === 'Completed'
+												? 'text-green-600'
+												: 'text-amber-600'
+										)}
+									>
+										{order.status}
+									</span>
+								</p>
 							</div>
 						</div>
-					</Link>
-				))}
-			</div>
-
-			<h2 className='text-xl font-medium mb-4'>Followed Stores</h2>
-
-			<div className='flex overflow-x-auto gap-4 py-1'>
-				{data.currentUser.followed.map((followed: any) => (
-					<Link
-						href={`/store/${followed.store.id}`}
-						key={followed.store.id}
-						className='flex flex-col items-center min-w-[80px]'
-					>
-						<div className='size-18 rounded-full overflow-hidden mb-2'>
-							{followed.store.image ? (
-								<img
-									src={followed.store.image?.path}
-									alt={followed.store.name}
-									className='size-full object-cover'
-								/>
-							) : (
-								<div className='size-full bg-muted-foreground/20 flex items-center justify-center'>
-									<span className='text-xl font-medium text-muted-foreground'>
-										{followed.store.name.charAt(0)}
-									</span>
-								</div>
-							)}
-						</div>
-						<span className='text-center truncate w-full font-medium'>
-							{followed.store.name}
-						</span>
-					</Link>
-				))}
-			</div>
+					</div>
+				</Link>
+			))}
 		</div>
 	);
 };
 
-const HOME_QUERY = gql`
-	query Home {
-		currentUser {
-			id
+const FollowedStores = () => {
+	const { isLoading, error, data } = useFollowedStoresQuery();
 
-			orders {
-				id
-				store {
-					id
-					name
-					image {
-						id
-						path
-					}
-				}
+	if (isLoading) return <div>Loading...</div>;
+	if (error) return <div>Error: {error.message}</div>;
+	if (!data) return null;
 
-				products {
-					orderId
-					productId
-					product {
-						id
-						name
-					}
-					unitPrice
-					quantity
-				}
-				total
-				status
-				createdAt
-			}
+	return (
+		<div className='flex overflow-x-auto gap-4 py-1'>
+			{data.stores.map(store => (
+				<Link
+					href={`/store/${store.id}`}
+					key={store.id}
+					className='flex flex-col items-center min-w-[80px]'
+				>
+					<div className='size-18 rounded-full overflow-hidden mb-2'>
+						{store.image ? (
+							<img
+								src={store.image?.path}
+								alt={store.name}
+								className='size-full object-cover'
+							/>
+						) : (
+							<div className='size-full bg-muted-foreground/20 flex items-center justify-center'>
+								<span className='text-xl font-medium text-muted-foreground'>
+									{store.name.charAt(0)}
+								</span>
+							</div>
+						)}
+					</div>
+					<span className='text-center truncate w-full font-medium'>
+						{store.name}
+					</span>
+				</Link>
+			))}
+		</div>
+	);
+};
 
-			followed {
-				storeId
-				followerId
-				store {
-					id
-					name
-					image {
-						id
-						path
-					}
-				}
-			}
+const HomePage = () => {
+	return (
+		<div>
+			<h1 className='text-2xl font-medium mb-4'>Recent Orders</h1>
 
-			watchlist {
-				userId
-				productId
-				product {
-					id
-					name
-					unitPrice
-					store {
-						id
-						name
-					}
-					images {
-						id
-						path
-					}
-				}
-			}
-		}
-	}
-`;
+			<RecentOrders />
+
+			<h2 className='text-xl font-medium mb-4'>Followed Stores</h2>
+
+			<FollowedStores />
+		</div>
+	);
+};
 
 export default HomePage;
