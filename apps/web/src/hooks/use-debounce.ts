3d5249510e6093ee6f7c74e12f4ea40391a9ffdd
Oluwakorede Fashokun <koredefashokun@gmail.com>
2025-10-18 13:48:31 -0400

    Make debounce implementation more solid

--- apps/web/src/hooks/use-debounce.ts
+++ apps/web/src/hooks/use-debounce.ts
@@ -1,23 +1,23 @@
 import React from 'react';
 import debounce from 'lodash.debounce';
 
-const useDebounce = (
-	callback: (...args: any[]) => void,
+const useDebounce = <F extends (...args: any[]) => any>(
+	callback: F,
 	delay: number = 1000
 ) => {
-	const ref = React.useRef<(...args: any[]) => void | null>(null);
+	const ref = React.useRef<F | null>(null);
 
 	React.useEffect(() => {
 		ref.current = callback;
 	}, [callback]);
 
 	const debouncedCallback = React.useMemo(() => {
-		const func = (...args: any[]) => {
+		const func = (...args: Parameters<F>) => {
 			ref.current?.(...args);
 		};
 
 		return debounce(func, delay);
-	}, []);
+	}, [delay]);
 
 	return debouncedCallback;
 };
