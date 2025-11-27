fba300ffcf2829223c43f77c8967698171129abf
Oluwakorede Fashokun <koredefashokun@gmail.com>
2025-11-27 03:02:23 -0500

    Use Promise.all instead of transation for search

--- api/src/core/data/search.ts
+++ api/src/core/data/search.ts
@@ -1,14 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
 export const globalSearch = async (prisma: PrismaClient, query: string) => {
-	const [products, stores] = await prisma.$transaction(async tx => [
-		tx.product.findMany({
+	const [products, stores] = await Promise.all([
+		prisma.product.findMany({
 			where: { name: { contains: query, mode: 'insensitive' } },
 			include: {
 				images: true
 			}
 		}),
-		tx.store.findMany({
+		prisma.store.findMany({
 			where: { name: { contains: query, mode: 'insensitive' } },
 			include: {
 				image: true
