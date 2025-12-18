1670cbea506469066589a3cfd2c50504024eb8e6
Oluwakorede Fashokun <koredefashokun@gmail.com>
2025-12-18 15:36:33 -0500

    Add styling to admin login screen

--- apps/admin/src/pages/auth/login.tsx
+++ apps/admin/src/pages/auth/login.tsx
@@ -12,6 +12,14 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useLoginMutation } from '@/data/mutations';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle
+} from '@/components/ui/card';
+import { cn } from '@/lib/utils';
 const loginSchema = z.object({
 	email: z.string().email(),
 	password: z.string().min(8)
@@ -33,42 +41,63 @@ const Login = () => {
 	};
 
 	return (
-		<div>
-			<Form {...form}>
-				<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
-					<FormField
-						control={form.control}
-						name='email'
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Email</FormLabel>
-								<FormControl>
-									<Input {...field} />
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
+		<div className='flex min-h-svh w-full items-center justify-center p-6 md:p-10'>
+			<div className='w-full max-w-sm'>
+				<div className={cn('flex flex-col gap-6')}>
+					<Card>
+						<CardHeader>
+							<CardTitle>Login to your account</CardTitle>
+							<CardDescription>
+								Enter your email below to login to your account
+							</CardDescription>
+						</CardHeader>
+						<CardContent>
+							<Form {...form}>
+								<form
+									onSubmit={form.handleSubmit(onSubmit)}
+									className='space-y-8'
+								>
+									<FormField
+										control={form.control}
+										name='email'
+										render={({ field }) => (
+											<FormItem>
+												<FormLabel>Email</FormLabel>
+												<FormControl>
+													<Input {...field} />
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
 
-					<FormField
-						control={form.control}
-						name='password'
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Password</FormLabel>
-								<FormControl>
-									<Input type='password' {...field} />
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
+									<FormField
+										control={form.control}
+										name='password'
+										render={({ field }) => (
+											<FormItem>
+												<FormLabel>Password</FormLabel>
+												<FormControl>
+													<Input type='password' {...field} />
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
 
-					<Button type='submit' disabled={loginMutation.isPending}>
-						Login
-					</Button>
-				</form>
-			</Form>
+									<Button
+										type='submit'
+										className='w-full'
+										disabled={loginMutation.isPending}
+									>
+										Login
+									</Button>
+								</form>
+							</Form>
+						</CardContent>
+					</Card>
+				</div>
+			</div>
 		</div>
 	);
 };
