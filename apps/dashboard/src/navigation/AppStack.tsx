9a5acaeb8c5e8d93d70850ac31437f18c7bc7bf0
Oluwakorede Fashokun <koredefashokun@gmail.com>
2026-02-02 16:23:09 -0500

    Flatten navigators into one file

--- apps/dashboard/src/navigation/AppStack.tsx
+++
@@ -1,8 +0,0 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import { AppStackParamList } from '../types/navigation';
-
-export const AppStack = createNativeStackNavigator<
-	AppStackParamList,
-	'AppStack'
->();
