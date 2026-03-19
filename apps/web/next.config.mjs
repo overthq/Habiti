55f5cd0505796bee089dab62dbff296cc0010ed1
Oluwakorede Fashokun <koredefashokun@gmail.com>
2026-03-19 14:47:08 -0400

    Maybe fix build?

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,5 +1,13 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	experimental: {
+		outputFileTracingRoot: path.join(__dirname, '../../')
+	},
 	images: {
 		remotePatterns: [
 			{
