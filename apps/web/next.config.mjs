dfe8275325ec40657cd9abeac649164dc5d82f3b
Oluwakorede Fashokun <koredefashokun@gmail.com>
2026-03-19 17:30:35 -0400

    Remove outputFileTracingRoot

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,11 +1,5 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	outputFileTracingRoot: path.join(__dirname, '../../'),
 	images: {
 		remotePatterns: [
 			{
