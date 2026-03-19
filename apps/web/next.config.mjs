e448997d024bfb5ff05a1ea9ca9b3939c9bc72b7
Oluwakorede Fashokun <koredefashokun@gmail.com>
2026-03-19 17:36:37 -0400

    Try with Turbopack root?

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,5 +1,13 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	turbopack: {
+		root: path.join(__dirname, '../../')
+	},
 	images: {
 		remotePatterns: [
 			{
