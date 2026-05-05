978bb8ca904ef349be431029811090a0b3c421ea
Oluwakorede Fashokun <koredefashokun@gmail.com>
2026-05-05 04:59:02 -0400

    Bump version

--- apps/dashboard/app.config.ts
+++ apps/dashboard/app.config.ts
@@ -6,7 +6,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
 	slug: 'habiti-dashboard',
 	scheme: 'habiti-dashboard',
 	owner: 'overthq',
-	version: '1.0.0',
+	version: '1.0.1',
 	orientation: 'portrait',
 	icon: './assets/icon.png',
 	updates: {
