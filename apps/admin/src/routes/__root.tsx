f6309238c8bb3e6c8745f5b3eef61ab238a8767c
Oluwakorede Fashokun <koredefashokun@gmail.com>
2026-09-01 13:49:08 -0400

    Suppress hydration warnings admin

--- apps/admin/src/routes/__root.tsx
+++ apps/admin/src/routes/__root.tsx
@@ -84,7 +84,7 @@ function RootComponent() {
 
 function RootDocument({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang='en'>
+		<html lang='en' suppressHydrationWarning>
 			<head>
 				<HeadContent />
 			</head>
